3f6d06afa7391fa9f9b7105f3df5eec6007017a0
kltyton <h3193778245@163.com>
2025-07-12 23:59:35 +0800

    serverFix-BY @OrzMiku

--- src/main/java/com/kltyton/polymorphic_tom/widget/CraftingTerminalWidget.java
+++ src/main/java/com/kltyton/polymorphic_tom/widget/CraftingTerminalWidget.java
@@ -2,8 +2,9 @@ package com.kltyton.polymorphic_tom.widget;
 
 import com.illusivesoulworks.polymorph.api.client.base.ITickingRecipesWidget;
 import com.illusivesoulworks.polymorph.client.recipe.widget.PlayerRecipesWidget;
-import com.kltyton.polymorphic_tom.client.SharedState;
+import com.kltyton.polymorphic_tom.mixin.AbstractStorageTerminalScreenAccessor;
 import com.kltyton.polymorphic_tom.mixin.CraftingTerminalBlockEntityAccessor;
+import com.kltyton.polymorphic_tom.network.packet.SelectRecipePacket;
 import com.tom.storagemod.gui.CraftingTerminalMenu;
 import com.tom.storagemod.gui.CraftingTerminalScreen;
 import com.tom.storagemod.tile.CraftingTerminalBlockEntity;
@@ -13,17 +14,45 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.Slot;
 
 public class CraftingTerminalWidget extends PlayerRecipesWidget implements ITickingRecipesWidget {
+    private final CraftingTerminalScreen screen;
+    private boolean lastTallMode;
+    private int lastRowCount;
+
     public CraftingTerminalWidget(CraftingTerminalScreen screen, Slot outputSlot) {
         super(screen, outputSlot);
+        this.screen = screen;
+
+        // 初始化最后的已知状态
+        AbstractStorageTerminalScreenAccessor accessor = (AbstractStorageTerminalScreenAccessor) screen;
+        this.lastTallMode = accessor.isTallMode();
+        this.lastRowCount = accessor.getRowCount();
     }
 
     @Override
     public void tick() {
+        AbstractStorageTerminalScreenAccessor accessor = (AbstractStorageTerminalScreenAccessor) screen;
+        boolean currentTallMode = accessor.isTallMode();
+        int currentRowCount = accessor.getRowCount();
+
+        // 检查更改并相应地更新
+        if (currentTallMode != lastTallMode || currentRowCount != lastRowCount) {
+            // 更新 Y 位置或执行其他必要的操作
+            updateWidgetPosition();
+
+            // 更新最后的已知状态
+            lastTallMode = currentTallMode;
+            lastRowCount = currentRowCount;
+        }
+    }
+
+    private void updateWidgetPosition() {
+        this.openButton.setOffsets(this.getXPos(), this.getYPos());
+        this.selectionWidget.setOffsets(this.getXPos() + this.xOffset, this.getYPos() + 41);
     }
 
     @Override
     public int getYPos() {
-        return super.getYPos() + 68;
+        return super.getYPos();
     }
 
     @Override
@@ -32,27 +61,13 @@ public class CraftingTerminalWidget extends PlayerRecipesWidget implements ITick
         Player player = Minecraft.getInstance().player;
         if (player != null) {
             if (player.containerMenu instanceof CraftingTerminalMenu) {
-                CraftingTerminalBlockEntity craftingBlockEntity = SharedState.currentCraftingTerminal;
-                if (craftingBlockEntity != null) {
-                    ((CraftingTerminalBlockEntityAccessor) craftingBlockEntity).setRefillingGrid(false);
-                    ((CraftingTerminalBlockEntityAccessor) craftingBlockEntity).setCurrentRecipe(null);
-                    ((CraftingTerminalBlockEntityAccessor) craftingBlockEntity).setReading(false);
-                    Minecraft.getInstance().execute(() -> {
-                        try {
-                            Thread.sleep(5);
-                            Minecraft.getInstance().execute(() -> ((CraftingTerminalBlockEntityAccessor) craftingBlockEntity).invokeOnCraftingMatrixChanged());
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    });
-                }
+                SelectRecipePacket.send();
             }
         }
     }
     @Override
     public void initChildWidgets() {
         super.initChildWidgets();
-        int openButtonYOffset = -68;
-        this.openButton.setOffsets(this.getXPos(), this.getYPos() + openButtonYOffset);
+        updateWidgetPosition();
     }
 }
